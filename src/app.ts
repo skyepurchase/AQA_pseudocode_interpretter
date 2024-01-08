9b8d9f3067dd66c1bd7e81e5195ca1cf0001f736
Skye Purchase <atp45@cam.ac.uk>
2024-01-08 14:12:37 +0000

    Test the lexer

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello World\!\!\!');
+import { isUndefined } from 'lodash';
+import { lexer } from './Grammar'
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Programme: ', (name: string) => {
+    lexer.reset(name);
+    let token: Object | undefined = lexer.next();
+    while (!isUndefined(token)) {
+        console.log(token);
+        token = lexer.next();
+    }
+    readline.close();
+});
